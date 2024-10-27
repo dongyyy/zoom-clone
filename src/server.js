dd99295e51b696f0f73d2184af22703f335bff0f
ethan <noahkim.j@gmail.com>
2024-10-27 23:10:11 +0900

    0.3 Frontend Setup

--- src/server.js
+++ src/server.js
@@ -2,6 +2,12 @@ import express from "express";
 
 const app = express();
 
+app.set("view engine", "pug");
+app.set("views", __dirname + "/views");
+app.use("/public", express.static(__dirname + "/public")); // http://localhost:3000/public/js/app.js 페이지 로딩
+
+app.get("/", (req, res) => res.render("home"));
 console.log("hello");
 
-app.listen(3000); // 포트 3000을 listen 해줄 것.
+const handleListen = () => console.log(`Listening on http:localhost:3000`);
+app.listen(3000, handleListen); // 포트 3000을 listen 해줄 것.
